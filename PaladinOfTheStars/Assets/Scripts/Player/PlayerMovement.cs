c05b1bc58ec86f6e5d8872f4dd8df20785804675
Justinas Grigas <34399460+SigSec@users.noreply.github.com>
2018-02-21 21:38:42 +0000

    Quick update

--- PaladinOfTheStars/Assets/Scripts/Player/PlayerMovement.cs
+++ PaladinOfTheStars/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Transform _playerTransform;
+    private Vector3 _mousePosition;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void Awake()
+    {
+        _playerTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        // First update the inputs
+        _horizontalInput = Input.GetAxisRaw("Horizontal") * 0.01f;
+        _verticalInput = Input.GetAxisRaw("Vertical") * 0.01f;
+        _mousePosition = Input.mousePosition;
+
+        // Update rotations
+        var _playerScreenPoint = Camera.main.WorldToScreenPoint(_playerTransform.transform.position);
+        if (_mousePosition.x < _playerScreenPoint.x)
+        {
+            _playerTransform.localScale = new Vector3(-1, _playerTransform.localScale.y, _playerTransform.localScale.z);
+        }
+        else
+        {
+            _playerTransform.localScale = new Vector3(1, _playerTransform.localScale.y, _playerTransform.localScale.z);
+        }
+
+        // Move the player
+        _playerTransform.position = new Vector3(_playerTransform.position.x + _horizontalInput, _playerTransform.position.y + _verticalInput, -1);
+    }
 }
