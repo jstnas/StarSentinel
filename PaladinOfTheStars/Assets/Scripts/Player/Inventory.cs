9bf00c0eb610ce544f3dcf1283d504e31613707c
Abu-Bakr <37830412+Abu-BakrRabbani@users.noreply.github.com>
2018-04-20 10:50:19 +0100

    Added Inventory

---
+++ PaladinOfTheStars/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    public List<Item> inventory = new List<Item>();
+    private ItemDatabase database; 
+
+
+	// Use this for initialization
+	void Start () {
+        database = GameObject.FindGameObjectsWithTag("Item Database").GetComponent<ItemDatabase>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
